9a9fbee2eac9ac1d71c27be9520cfdbf20cec2d0
BigBoss209 <2094585084@qq.com>
2019-11-25 20:12:30 +0800

    helloworld

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
   public static void main(String[] args) {
+	   System.out.println("2");
+        System.out.println("hello world");
         System.out.println("1");
         System.out.println("hello world");
-        System.out.println("2");
-        System.out.println("hello world");
+       
     }
